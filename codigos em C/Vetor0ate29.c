6faa6c3400f6b31e987a62ed9dc648e359307eae
Anáile M.C Gonçalves <anailemgoncalves@gmail.com>
2018-06-06 13:55:11 -0300

    adicionando Vetor que imprime de 0 ate 29

---
+++ codigos em C/Vetor0ate29.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+int main (){
+
+    int numero[30];
+
+    for(int i=0; i<30;i++){
+        printf("%i\n",numero[30]);
+        }
+    }
+
+
+
+
+
+
+
+
